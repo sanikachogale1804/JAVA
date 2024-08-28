942b3ddfea582e5989d71ceaf1edcd2844bcf582
unknown <chogalesanika8@gmail.com>
2024-08-28 08:59:26 +0530

    initial

---
+++ Datatype.java
@@ -0,0 +1,22 @@
+class Datatype
+{
+	public static void main(String args[])
+	{
+		// identifier matlab jo hum hamare taraf se likhte hai(classname,variablename)
+		byte sanika=50;
+		System.out.println("value of byte is: "+sanika);
+		System.out.println("**********"+sanika+"************");
+		System.out.println(sanika+" is stored in a variable");
+		
+		short a=20000;
+		System.out.println("value of a is: "+a);
+		
+		int b=27762665;
+		System.out.println("value of b is: "+b);
+		
+		float c=12.96f;
+		System.out.println("value of c is: "+c);
+		
+	}
+	
+}
---
+++ ThirdProgram.java
@@ -0,0 +1,9 @@
+class ThirdProgram
+{
+    public static void main(String args[])
+	{
+	    System.out.println("******************************");
+		System.out.println("       Sanika Chogale");
+		System.out.println("******************************");
+	}
+}
