87f07a731b6e86fad88f7993597f7e122118ac6d
unknown <chogalesanika8@gmail.com>
2024-08-29 08:04:15 +0530

    initial

---
+++ AreaOfRectangle.java
@@ -0,0 +1,13 @@
+class AreaOfRectangle
+{
+	public static void main(String args[])
+	{
+		int length=10;
+		int breadth=20;
+		
+		System.out.println("Area of rectangle is:"+(length*breadth));
+		
+		int side=20;
+		System.out.println("Area of square is:"+(side*side));
+	}
+}
