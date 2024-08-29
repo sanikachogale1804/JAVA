f967842d12394f46dcefb42065db76ed14520b6e
unknown <chogalesanika8@gmail.com>
2024-08-29 07:56:15 +0530

    initial

--- addition.java
+++ addition.java
@@ -2,11 +2,15 @@ class addition
 {
 	public static void main (String args[])
 	{
-		int a=100;
+		int a=400;
 		int b=200;
 		
-		System.out.println("addition is: "+(a+b));
-		
+		// when we doing addition we should put the whole no together in () otherwise it consider the string while we want output integer
+		System.out.println("addition is: "+(a+b)); // a and b are operands and + is a operator
+        System.out.println("subtraction is:"+(a-b));
+		System.out.println("multiplication is:"+(a*b));
+		System.out.println("dividion is:"+(a/b));
+		System.out.println("module is:"+(a%b));
 	}
 	
 }
