5d0b1c1dccc498363b4685858bb539e0f40866a9
unknown <chogalesanika8@gmail.com>
2024-08-30 08:48:17 +0530

    initial

---
+++ LogicalOperator.java
@@ -0,0 +1,43 @@
+class LogicalOperator
+{
+	public static void main (String args[])
+	{
+		int a=50;
+		System.out.println(a!=0 && a==10);
+		System.out.println(a==0 && a>=10);
+		System.out.println(a%50==0 || a%15==0);
+		System.out.println(a>=0 || a==10);
+		
+		// not(!) operator(it is calles unary operaor also)
+		System.out.println(!(a>=0 || a==10));
+		
+		//Bitwise Operator
+		//Bitwise and   &
+		//Bitwise OR    |
+		//Bitwise XOR   ^
+		//if bits are deifferent then 1 
+		//bits same hai to 0 milega
+		//Bitwise NOt   ~
+		
+	    System.out.println(20&12);        //20=10100
+		                                  //10=01100
+                                          //- --------
+										  //   00100 
+        System.out.println(14&8);         //14=1110
+		                                  //8= 1000
+										  // ---------
+										  //   1000 
+        System.out.println(20|12);        //20=10100
+		                                  //10=01100
+                                          //- --------
+										  //   11100 
+        System.out.println(14^8);         //14=1110
+		                                  //8= 1000
+										  // ---------
+										  //   0110
+	    System.out.println(~14);           //0000 0000 0000 1110
+		                                   //1111 1111 1111 0001
+	    System.out.println(~8);           //0000 0000 0000 1000
+		                                   //1111 1111 1111 0111  
+	}
+}
--- ThirdProgram.java
+++ ThirdProgram.java
@@ -4,6 +4,13 @@ class ThirdProgram
 	{
 	    System.out.println("******************************");
 		System.out.println("       Sanika Chogale");
-		System.out.println("******************************");
+		System.out.println("******************************");ya
+		// print ke andar kuch value pass nahi ki to error aayega kyuki java ne aaisa koi function nahi bana
+		// println me argument nahi diya to bhi chalega
 	}
 }
+
+// to declare variable syntax
+// datatype variable_name;(if we don't want to give value)
+// datatype variable_name=values;
+//  int         a        = 6;
