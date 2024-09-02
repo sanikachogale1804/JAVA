19554bac2690ddd84695a6785f1745e387ba6c6b
unknown <chogalesanika8@gmail.com>
2024-09-02 08:49:34 +0530

    initial

---
+++ p7.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class p7
+{
+	public static void main(String args[])
+	{
+		Scanner sc=new Scanner(System.in);
+		
+		System.out.println("Enter name");
+		String name=sc.next();
+		System.out.println(name);
+		
+		System.out.println("Enter age");
+		int age=sc.nextInt();
+		System.out.println(age);
+		
+	}
+}
