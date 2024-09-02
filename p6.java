de5cbf7849e895c9895a3092b799408e3324a33b
unknown <chogalesanika8@gmail.com>
2024-09-02 08:42:48 +0530

    initial

---
+++ p6.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;  //import karna padta hai nahi to scanner kya hai vo pata nahi chalega
+//import ke andar scanner class hai
+class p6
+{
+	public static void main(String args[])
+	{
+		Scanner sc=new Scanner (System.in);
+		
+		String sentence=sc.nextLine();
+		System.out.println("sentence is:"+sentence);
+	}
+}
