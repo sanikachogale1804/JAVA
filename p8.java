7c509ff5d7120c57b75d6178e85e334e5ebffee1
unknown <chogalesanika8@gmail.com>
2024-09-03 08:09:08 +0530

    initial

---
+++ p8.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+class p8
+//charAt matlab character at some index
+{
+	public static void main(String args[])
+	{
+		Scanner sc=new Scanner(System.in);
+		boolean subscribed=false;
+		System.out.println("Do you want to subscribe??Y/N");
+		char decision=sc.next().charAt(0);
+		
+		if(decision=='y' || decision=='Y')
+		{
+			subscribed=true;
+		}
+		System.out.println("Subscribed:"+subscribed);
+	}
+	
+}
+
