bf119c313b39353b46da119caa66892bf3afb96e
unknown <chogalesanika8@gmail.com>
2024-09-03 08:36:59 +0530

    initial

--- IfElse.java
+++ IfElse.java
@@ -1,14 +1,28 @@
+import java.util.*;
 class IfElse
 {
 	public static void main (String args[])
-	{
-		if(true)
+	{		
+	
+	    Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+	 
+		if(n%2==0)
 		{
-			System.out.println("If block");
+			System.out.println(n+" is Even");
 		}
 		else
 		{
-			System.out.println("else block");
+			System.out.println(n+" is Odd");
+		}
+		
+		if(n%7==0)
+		{
+			System.out.println(n+" is multiple by 7");
+		}
+		else
+		{
+			System.out.println(n+" is not multiple by 7");
 		}
 	}
 }
