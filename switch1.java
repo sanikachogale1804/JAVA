fbb2fc346439ea1a8dd1fe0dbf14109a3cd20f7b
unknown <chogalesanika8@gmail.com>
2024-09-05 07:58:19 +0530

    initial

--- switch1.java
+++ switch1.java
@@ -2,7 +2,7 @@ class switch1
 {
 	public static void main(String args[])
 	{
-		switch(1)
+		switch(112)
 		{
 			case 1:
 			   System.out.println("case 1");
@@ -13,6 +13,9 @@ class switch1
 			case 3:
 			   System.out.println("case 3");
 			   break;
+			   
+			default:
+			   System.out.println("No switching case");
 		}
 	}
 }
