47b5489689f7d86ea3474e800561789bfe22c131
unknown <chogalesanika8@gmail.com>
2024-09-05 08:15:47 +0530

    initial

--- switch1.java
+++ switch1.java
@@ -1,8 +1,9 @@
+import java.util.Scanner;
 class switch1
 {
 	public static void main(String args[])
 	{
-		switch(112)
+		/*switch(112)
 		{
 			case 1:
 			   System.out.println("case 1");
@@ -16,6 +17,39 @@ class switch1
 			   
 			default:
 			   System.out.println("No switching case");
+			   
+		}*/
+		
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		System.out.println("1:monday 2:Tuesday 3:wednesday 4:thursday 5:friday 6:saturday 7:sunday");
+		switch(n)
+		{
+			case 1:
+			   System.out.println("Monday");
+			   break;
+			case 2:
+			   System.out.println("Tuesday");
+			   break;
+			case 3:
+			   System.out.println("Wednesday");
+			   break;
+			case 4:
+			   System.out.println("Thursday");
+			   break;
+			case 5:
+			   System.out.println("Friday");
+			   break;
+			case 6:
+			   System.out.println("Saturday");
+			   break;
+			case 7:
+			   System.out.println("Sunday");
+			   break;
+			default:
+			   System.out.println("No switching case");
+			   
 		}
+		
 	}
 }
