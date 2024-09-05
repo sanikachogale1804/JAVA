976bd8e6d729b0ba8781428eb2babe91451adcf7
unknown <chogalesanika8@gmail.com>
2024-09-05 07:56:26 +0530

    initial

---
+++ switch.java
@@ -0,0 +1,15 @@
+class switch
+{
+	public static void main(String args[])
+	{
+		switch(1)
+		{
+			case 1:
+			   System.out.println("case 1");
+			case 2:
+			   System.out.println("case 2");
+			case 3:
+			   System.out.println("case 3");
+		}
+	}
+}
---
+++ switch1.java
@@ -0,0 +1,18 @@
+class switch1
+{
+	public static void main(String args[])
+	{
+		switch(1)
+		{
+			case 1:
+			   System.out.println("case 1");
+			   break;
+			case 2:
+			   System.out.println("case 2");
+			   break;
+			case 3:
+			   System.out.println("case 3");
+			   break;
+		}
+	}
+}
