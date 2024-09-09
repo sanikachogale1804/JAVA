a510d23ab9be5c55dc120d28eafabded603c0c24
unknown <chogalesanika8@gmail.com>
2024-09-09 08:39:03 +0530

    initial

---
+++ while3.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class while3
+{
+	public static void main (String args[])
+	{
+		Scanner sc=new Scanner(System.in);
+		int n=3;
+		
+		while(n>=0)
+		{
+			n=sc.nextInt();
+		}
+	}
+}
