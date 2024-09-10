7782e391351269df9c5a3ef55662ea4d7e94998d
unknown <chogalesanika8@gmail.com>
2024-09-10 08:01:02 +0530

    initial

--- forLoop.java
+++ forLoop.java
@@ -10,9 +10,14 @@ class forLoop
 		{
 			System.out.println(i);
 		}
-		for(int i=30;i>=20;i--)
+		for(int i=13;i<=20;i++)
 		{
-			System.out.print(i);
+			System.out.print(i+" ");
+		}
+		System.out.println();
+		for(int i=21;i>=13;i--)
+		{
+			System.out.print(i+" ");
 		}
 	}
 }
