8483ab0dddf6bdc46321146da4ff71ce816af58f
unknown <chogalesanika8@gmail.com>
2024-09-12 08:16:14 +0530

    initial

--- p11.java
+++ p11.java
@@ -4,26 +4,27 @@ class p11
 	public static void main(String args[])
 	{
 		Scanner sc=new Scanner(System.in);
-		int n=sc.nextInt();
 		
-		int count=0;
+		for(int n=1;n<=100;n++)
+		{
+			int count=0;
 		for (int i=1;i<=n;i++)
 		{
 			if(n%i==0)
 			{
-				System.out.print(i+" ");
+				
 				count++;
 			}
 		}
-	    System.out.print("count:"+count);	
+	   
 
         if (count==2)		
 		{
 			System.out.println(n+" is prime");
 		}
-		else
-		{
-			System.out.println(n+" is not prime");
+		
 		}
+		
+		
 	}
 }
