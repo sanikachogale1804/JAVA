6fd43df442777996df681f813c8fb51b8c3200b2
unknown <chogalesanika8@gmail.com>
2024-09-12 07:54:22 +0530

    initial

--- PrimeNumber.java
+++ PrimeNumber.java
@@ -2,14 +2,16 @@ class PrimeNumber
 {
 	public static void main(String args[])
 	{
-		int n=20;
+		int n=100;
+		int count=0;
 		for (int i=1;i<=n;i++)
 		{
 			if(n%i==0)
 			{
-				System.out.println(i+" ");
+				System.out.print(i+" ");
+				count++;
 			}
 		}
-			
+	    System.out.print("count:"+count);		
 	}
 }
