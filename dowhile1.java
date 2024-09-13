000f9ee43331aeeaaa3af1925d235685e58f2673
unknown <chogalesanika8@gmail.com>
2024-09-13 07:34:42 +0530

    initial

---
+++ dowhile1.java
@@ -0,0 +1,10 @@
+class dowhile1
+{
+	public static void main(String args[])
+	{
+		do
+		{
+			System.out.println("Hello");
+		}while(false);
+	}
+}
---
+++ p12.java
@@ -0,0 +1,13 @@
+class p12
+{
+	public static void main(String args[])
+	{
+		int n=7;
+		for(int=1;i<=10;i++)
+		{
+			System.out.println(n*i);
+			System.out.println("Hello"*i);
+		}
+		
+	}
+}
