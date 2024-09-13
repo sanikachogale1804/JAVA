ce38c663c49c78bcc7007119f4c2d99861e2eecb
unknown <chogalesanika8@gmail.com>
2024-09-13 07:42:37 +0530

    initial

--- dowhile1.java
+++ dowhile1.java
@@ -1,10 +1,15 @@
+import java.util.Scanner;
 class dowhile1
+//do me likho apko kya print karna hai aur while me condition likho kab tak print karna hai
 {
 	public static void main(String args[])
 	{
+		Scanner sc=new Scanner(System.in);
+		int n;
 		do
 		{
 			System.out.println("Hello");
-		}while(false);
+			n=sc.nextInt();
+		}while(n==1);
 	}
 }
