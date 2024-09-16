a7918336f55d2ada2a00f302fef8fcbc35e0d2cd
unknown <chogalesanika8@gmail.com>
2024-09-16 07:56:38 +0530

    initial

--- p13.java
+++ p13.java
@@ -2,7 +2,12 @@ class p13
 {
 	public static void main(String args[])
 	{
-		for(int i=65;i<=90;i++)    //65+25=90 25 alphabets
+		/*for(int i=65;i<=90;i++)    //65+25=90 25 alphabets
+		{
+			System.out.println((char)i);
+		}*/
+		
+		for(int i=97;i<=122;i++)    
 		{
 			System.out.println((char)i);
 		}
