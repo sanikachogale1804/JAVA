edf9cc560af6e4176a7e3df2e94b783fa5dac8e8
unknown <chogalesanika8@gmail.com>
2024-09-16 07:33:54 +0530

    initial

---
+++ p13.java
@@ -0,0 +1,21 @@
+class p13
+{
+	public static void main(String args[])
+	{
+		//Typecasting
+		//Typecasting matlab ek datatype ko dusre datatype me convert karna hai
+		//widening typecasting matlab chote se bada jate ho matlab small container put in big container
+		//widening matlab bade datatype me chota datatype ye java compiler khud kar leta hai par narrowing me hame datatype convert karna padta hai
+		
+		int a=100;
+		long b=a;
+		System.out.println(a);
+		
+		//narrowing casting bade datatype se chote datatype me 
+		
+		int c=123;
+		byte d=(byte)c;
+		System.out.println(d);
+		
+	}
+}
