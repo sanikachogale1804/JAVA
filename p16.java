879dd6812115f1a51c1a5f4846b60fb78479a69e
unknown <chogalesanika8@gmail.com>
2024-09-17 07:35:27 +0530

    initial

---
+++ p16.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class p16
+{
+	public static void main(String args[])
+	{
+		Scanner sc=new Scanner(System.in);
+		String weekdays[]={"mon","Tue","wed","thurs","fri","sat","sun"};
+		int primenumbers[]={11,13,17,5};
+		
+		System.out.println("weekdays:"+weekdays.length);
+		System.out.println("primenumbers:"+primenumbers.length);
+		
+	}
+}
