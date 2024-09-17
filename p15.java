d56b8d31dd5ac1cacbdfae47344a6e9f2f7fb4fc
unknown <chogalesanika8@gmail.com>
2024-09-17 07:23:06 +0530

    initial

---
+++ p15.java
@@ -0,0 +1,13 @@
+//command line arguments
+//jo bhi value aap runtime pe pass karte ho usko command line bolte hai
+import java.util.Scanner;
+class p15
+{
+	public static void main(String args[])
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println(args[0]);
+		System.out.println(args[1]);
+		System.out.println(args[2]);
+	}
+}
