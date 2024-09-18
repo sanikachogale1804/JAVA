44ef58f41d4638466c4871290312c7b5f40ea421
unknown <chogalesanika8@gmail.com>
2024-09-18 08:42:06 +0530

    initial

--- p18.java
+++ p18.java
@@ -13,18 +13,23 @@ class p18
 			 System.out.println(n.length);
 		 }
 		 System.out.println("=====================");
-		 
+		 //done by enhance loop (iterations)
 		 String courses[][]={
 			 {"HTML","CSS","JAVASCRIPT"},
 			 {"MYSQL","MONGODB"},
 			 {"CORE JAVA","SPRINGBOOT"}};
-			 	 
+			
+        int total_courses=0;			
 		for(String ch[]:courses)
 		{
+			total_courses+=ch.length;
 			for(String cour:ch)
 			{
 				System.out.println(cour);
+				//total_courses++;
 			}
 		}
+		System.out.println("total_courses:"+total_courses);
+		
 	}
 }
