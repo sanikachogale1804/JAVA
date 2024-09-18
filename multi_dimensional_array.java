8950dc8925dbb1010119463a8623554e8288de3c
unknown <chogalesanika8@gmail.com>
2024-09-18 07:29:09 +0530

    initial

---
+++ multi_dimensional_array.java
@@ -0,0 +1,13 @@
+class multi_dimensional_array
+{
+    public static void main(String args[])
+    {
+         //one dimensional               multi dimensional(two,three,four dimensional)
+		 int numbers[][]={{12,23,56},{45,23,78}};
+		  //                  0          1
+							   
+		System.out.println(numbers[0][0]);		
+		System.out.println(numbers[1][2]);		
+        System.out.println(numbers[0][1]);		
+    }
+}
