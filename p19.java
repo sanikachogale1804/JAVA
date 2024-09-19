6b13572a48cad7565c34e18cdec0c430844bb901
unknown <chogalesanika8@gmail.com>
2024-09-19 08:14:00 +0530

    initial

--- p19.java
+++ p19.java
@@ -22,10 +22,16 @@ class p19
 {
 	public static void main(String args[])
 	{
-	    Student s1=new Student();	
+	    Student s1=new Student();
+        Student s2=new Student();		
 		s1.rollNo=(101);
 		s1.name=("Sanika");
+		s2.rollNo=102;
+		s2.name="sneha";
+		
 		System.out.println(s1.rollNo);
 		System.out.println(s1.name);
+		System.out.println(s2.rollNo);
+		System.out.println(s2.name);
 	}
 }
