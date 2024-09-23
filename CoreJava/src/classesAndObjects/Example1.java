1741a0dc0ad2aa24131edc1bb1d000afc93ff223
unknown <chogalesanika8@gmail.com>
2024-09-23 08:29:45 +0530

    initial

---
+++ CoreJava/src/classesAndObjects/Example1.java
@@ -0,0 +1,12 @@
+package classesAndObjects;
+
+public class Example1 {
+
+	public static void main(String[] args) {
+		Order o1= new Order();
+		o1.OrderDetails(101,"T-shirt",500);
+		
+		Order o2=new Order();
+		o2.OrderDetails(102,"watch",700);
+	}
+}
--- CoreJava/src/classesAndObjects/Order.java
+++ CoreJava/src/classesAndObjects/Order.java
@@ -6,4 +6,12 @@ public class Order {
 	String orderName;
 	int orderPrice;
 
+	void OrderDetails(int orderId,String orderName,int orderPrice)
+	{
+		this.orderId=orderId;
+		this.orderName=orderName;
+		this.orderPrice=orderPrice;
+		
+		System.out.println(this.orderId+" "+this.orderName+" "+this.orderPrice);
+	}
 }
