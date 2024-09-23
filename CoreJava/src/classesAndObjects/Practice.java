c525dba55862acfe089f604e47b5bc00846eb51e
unknown <chogalesanika8@gmail.com>
2024-09-23 20:27:37 +0530

    initial

---
+++ CoreJava/src/classesAndObjects/Practice.java
@@ -0,0 +1,23 @@
+package classesAndObjects;
+
+public class Practice {
+	
+	int id;
+	String name;
+	
+	void setdetails()
+	{
+		System.out.println("details:");
+		System.out.println(id+" "+name+" ");
+	}
+	
+	int printid()
+	{
+		return id;
+	}
+	
+	
+	
+}
+
+
---
+++ CoreJava/src/classesAndObjects/Practice1.java
@@ -0,0 +1,21 @@
+package classesAndObjects;
+
+public class Practice1 {
+	
+	public static void main(String[] args) {
+		
+	   Practice p1=new Practice();
+	    p1.id=1;
+		p1.name="Mobile";
+		
+		System.out.println(p1.id);
+		System.out.println(p1.name);
+		
+		p1.setdetails();
+		p1.printid();
+	   
+	}
+	
+   
+
+}
