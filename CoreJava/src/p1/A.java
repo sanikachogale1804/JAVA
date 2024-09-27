76fddf3c9f71950c4ac72fd6cf4964b36bfaeffe
unknown <chogalesanika8@gmail.com>
2024-09-27 19:08:35 +0530

    initial

---
+++ CoreJava/src/p1/A.java
@@ -0,0 +1,14 @@
+package p1;
+
+public class A {
+
+	void methodA()
+	{
+		System.out.println("Method of A");
+	}
+	public static void main(String[] args) {
+		A A1=new A();
+		A1.methodA();
+	}
+	
+}
---
+++ CoreJava/src/p1/B.java
@@ -0,0 +1,10 @@
+package p1;
+
+public class B 
+{
+	public static void main(String[] args) {
+		B b1=new B();
+		b1.methodA();
+	}
+
+}
