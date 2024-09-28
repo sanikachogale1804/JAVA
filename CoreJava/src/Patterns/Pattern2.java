881b151858a6d11e5826fc1e3c3bcf4b11dbe24f
unknown <chogalesanika8@gmail.com>
2024-09-28 08:19:05 +0530

    initial

--- CoreJava/src/Patterns/Pattern2.java
+++ CoreJava/src/Patterns/Pattern2.java
@@ -1,24 +1,44 @@
 package Patterns;
 
+class pattern{
+	
+	void squareframe()
+	{
+		int n=10;
+		
+		{
+			for(int j=1;j<=n;j++)
+			{
+				for(int i=1;i<=n;i++)
+				{
+					if(i==1 || j==1 || i==n || j==n || i==2 || j==2 || i==n-1 || j==n-1 || i==3 || j==3 || i==n-2 || j==n-2)
+					{
+						System.out.print("* ");
+					}
+					else {
+						System.out.print("  ");
+					}
+				}
+				
+				System.out.println();
+			}
+			
+	}
+}
+}
+
 public class Pattern2 {
 
 	public static void main(String[] args) {
 		
-		int n=10;
-		for(int j=1;j<=n;j++)
+		pattern p=new pattern();
+		
+		for(int i=1;i<=10;i++)
 		{
-			for(int i=1;i<=n;i++)
-			{
-				if(i==1 || j==1 || i==n || j==n || i==2 || j==2 || i==n-1 || j==n-1 || i==3 || j==3 || i==n-2 || j==n-2)
-				{
-					System.out.print("* ");
-				}
-				else {
-					System.out.print("  ");
-				}
-			}
+			p.squareframe();
 			System.out.println();
 		}
+		
 		
 	}
 	
