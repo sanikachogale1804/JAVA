38aa49a17682f7e296f425a0a4408173709f87f1
unknown <chogalesanika8@gmail.com>
2024-09-28 07:42:41 +0530

    initial

--- CoreJava/src/Patterns/Pattern2.java
+++ CoreJava/src/Patterns/Pattern2.java
@@ -2,4 +2,18 @@ package Patterns;
 
 public class Pattern2 {
 
+	public static void main(String[] args) {
+		
+		
+		for(int j=1;j<=5;j++)
+		{
+			for(int i=1;i<=5;i++)
+			{
+				System.out.print(j);
+			}
+			System.out.println();
+		}
+		
+	}
+	
 }
