b6868f10c9d7bd1bad12b63d94cca8f42a0517fd
unknown <chogalesanika8@gmail.com>
2024-09-28 08:46:42 +0530

    initial

--- CoreJava/src/Patterns/Pattern3.java
+++ CoreJava/src/Patterns/Pattern3.java
@@ -3,7 +3,7 @@ package Patterns;
 public class Pattern3 {
 
 	public static void main(String[] args) {
-		int n=10;
+		int n=4;
 		for(int i=1;i<=n;i++)
 		{
 			for(int j=1;j<=i;j++)
@@ -12,6 +12,98 @@ public class Pattern3 {
 			}
 			System.out.println();
 		}
+		
+		
+		for(int i=1;i<=n;i++)
+		{
+			for(int j=1;j<=i;j++)
+			{
+				System.out.print(i);
+			}
+			System.out.println();
+		}
+		System.out.println("====================");
+		
+		for(int i=1;i<=n;i++)
+		{
+			for(int j=1;j<=i;j++)
+			{
+				System.out.print(j);
+			}
+			System.out.println();
+		}
+		System.out.println("========================");
+		
+		for(int i=1;i<=n;i++)
+		{
+			for(int j=1;j<=i;j++)
+			{
+				if(i==3)
+				{
+					System.out.print(i);
+				}
+				else
+				{
+					System.out.print(j);
+				}
+			}
+			System.out.println();
+		}
+		System.out.println("========================");
+		
+		for(int i=1;i<=n;i++)
+		{
+			for(int j=1;j<=i;j++)
+			{
+				if(i==1 || i==3)
+				{
+					System.out.print("*");
+				}
+				else
+				{
+					System.out.print(i);
+				}
+			}
+			System.out.println();
+		}
+		System.out.println("========================");
+		
+		for(int i=1;i<=n;i++)
+		{
+			for(int j=1;j<=i;j++)
+			{
+				if(i==2 || i==4)
+				{
+					System.out.print("*");
+				}
+				else
+				{
+					System.out.print(j);
+				}
+			}
+			System.out.println();
+		}
+		System.out.println("========================");
+		
+		for(int i=1;i<=n;i++)
+		{
+			for(int j=1;j<=i;j++)
+			{
+				if(i==2 || i==4)
+				{
+					System.out.print("0");
+				}
+				else
+				{
+					System.out.print(j);
+				}
+			}
+			System.out.println();
+		}
+		System.out.println("========================");
+		
+		
+		
 	}
 	
 	
