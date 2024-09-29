ba133bd8ece17ef248c3b0756b81da420c3fee04
unknown <chogalesanika8@gmail.com>
2024-09-29 19:10:58 +0530

    initial

--- CoreJava/src/Polymorphism/calculatorpractice.java
+++ CoreJava/src/Polymorphism/calculatorpractice.java
@@ -12,11 +12,17 @@ public class calculatorpractice {
 		System.out.println(a+b+b);
 	}
 	
+	void sub(float a,float b)
+	{
+		System.out.println(a-b);
+	}
+	
 	public static void main(String[] args) {
 		 
 		calculatorpractice cp=new calculatorpractice();
 		cp.add(12,60);
 		cp.add(30F,20F);
+		cp.sub(50,20);
 	}
 	
 }
