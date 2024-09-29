e0be0955bbd99baf8851b0a7547e64420941019f
unknown <chogalesanika8@gmail.com>
2024-09-29 19:03:17 +0530

    initial

--- CoreJava/src/Patterns/Pattern5.java
+++ CoreJava/src/Patterns/Pattern5.java
@@ -7,10 +7,30 @@ public class Pattern5 {
 		int n=6;
 		for(int i=1;i<=n;i++)
 		{
+			for(int j=1;j<=n-i;j++)
+			{
+				System.out.print(" ");
+			}
 			for(int j=1;j<=i;j++)
 			{
-				System.out.print(i);
+				System.out.print(i+" ");
 			}
+			
+			System.out.println();
+		}
+		
+		
+		for(int i=1;i<=n;i++)
+		{
+			for(int j=1;j>=2*(n-i);j--)
+			{
+				System.out.print(" ");
+			}
+			for(int j=1;j<=i;j++)
+			{
+				System.out.print(i+" ");
+			}
+			
 			System.out.println();
 		}
 		
