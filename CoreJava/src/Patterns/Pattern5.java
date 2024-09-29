c4cf08b2377c9cf02ea0ba76b8fb704e56f67182
unknown <chogalesanika8@gmail.com>
2024-09-29 19:07:15 +0530

    initial

--- CoreJava/src/Patterns/Pattern5.java
+++ CoreJava/src/Patterns/Pattern5.java
@@ -17,24 +17,7 @@ public class Pattern5 {
 			}
 			
 			System.out.println();
-		}
-		
-		
-		for(int i=1;i<=n;i++)
-		{
-			for(int j=1;j>=2*(n-i);j--)
-			{
-				System.out.print(" ");
-			}
-			for(int j=1;j<=i;j++)
-			{
-				System.out.print(i+" ");
-			}
-			
-			System.out.println();
-		}
-		
-		
+		}	
 	}
 	
 }
---
+++ CoreJava/src/Polymorphism/calculatorpractice.java
@@ -0,0 +1,16 @@
+package Polymorphism;
+
+public class calculatorpractice {
+
+	void add(int a,int b)
+	{
+	System.out.println(a+b);
+	}
+	
+	public static void main(String[] args) {
+		 
+		calculatorpractice cp=new calculatorpractice();
+		cp.add(12,60);
+	}
+	
+}
