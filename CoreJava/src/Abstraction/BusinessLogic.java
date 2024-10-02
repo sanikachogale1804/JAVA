170cb56a4eba160431793eaa6bdcff58dd415ff2
unknown <chogalesanika8@gmail.com>
2024-10-02 19:00:35 +0530

    initial

--- CoreJava/src/Abstraction/BusinessLogic.java
+++ CoreJava/src/Abstraction/BusinessLogic.java
@@ -20,18 +20,3 @@ class weekends extends BusinessLogic
 			System.out.println("weeknds coupons:");
 		}
 }
-import java.util.Scanner;
-class continue1
-{
-	public static void main(String args[])
-	{
-		Scanner sc=new Scanner(System.in);
-		for(int i=1;i<=5;i++)
-		{
-			if(i==3)
-				continue;
-			System.out.println(i);
-		}
-		
-	}
-}
