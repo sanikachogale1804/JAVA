c79a67767e6c2b6a8ca324f7f9ff40de2b989463
unknown <chogalesanika8@gmail.com>
2024-10-04 08:24:56 +0530

    initial

--- CoreJava/src/Abstraction/InterfaceEx6.java
+++ CoreJava/src/Abstraction/InterfaceEx6.java
@@ -5,6 +5,16 @@ interface I6
 {
 	void printname(String name);
 }
+@FunctionalInterface
+interface I7
+{
+	int add(int a,int b);
+}
+@FunctionalInterface
+interface I8
+{
+	int square(int n);
+}
 
 public class InterfaceEx6 {
 	
@@ -23,6 +33,13 @@ public class InterfaceEx6 {
 		};
 		obj1.printname("riya");
 		
+		I7 obj2=(a,b)->a+b;
+	    int sum=obj2.add(12,20);
+	    System.out.println(sum);
+		System.out.println(obj2.add(160,10));
+		
+		I8 obj3=(n)->n*n;
+		System.out.println(obj3.square(5));
 	}
 
 }
