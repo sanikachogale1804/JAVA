d15a2c9a8660e4885726ad60600d35799940de17
unknown <chogalesanika8@gmail.com>
2024-10-04 08:10:11 +0530

    initial

--- CoreJava/src/Abstraction/InterfaceEx6.java
+++ CoreJava/src/Abstraction/InterfaceEx6.java
@@ -11,8 +11,17 @@ public class InterfaceEx6 {
 	public static void main(String[] args) {
 		
 		//agar code ek line ka to sysout likh do or 2 se jyada line hai to curly braces me likho
+		//obj is name of object and name is variable
 		I6 obj=(name)->System.out.println("My name is: "+name);
 		obj.printname("sanika");
+		
+		I6 obj1=(name)->{
+			for(int i=1;i<=100;i++)
+			{
+				System.out.println(name);
+			}
+		};
+		obj1.printname("riya");
 		
 	}
 
