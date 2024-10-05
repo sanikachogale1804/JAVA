367424f5c94ea7aea908a0a8b1f51828630fdd89
unknown <chogalesanika8@gmail.com>
2024-10-05 19:07:22 +0530

    initial

--- CoreJava/src/Abstraction/InterfaceEx7.java
+++ CoreJava/src/Abstraction/InterfaceEx7.java
@@ -4,22 +4,29 @@ interface Practice
 {
 	void printNum(int n);
 }
-
+@FunctionalInterface
+interface Practice1
+{
+	void printName(String name);
+}
 public class InterfaceEx7 {
 
 	public static void main(String[] args) {
 		
-		Practice p=new Practice() {
+		/*Practice p=new Practice() {
 			public void printNum(int n)
 			{
 				System.out.println(n);
 			}
 		};
 		
-		p.printNum(12);
-		
-		/*Practice p=(n)->System.out.println( n);
 		p.printNum(12);*/
+		
+		Practice p=(n)->System.out.println(n);
+		p.printNum(12);
+	
+		Practice1 p1=(name)->System.out.println(name);
+		p1.printName("my name is sanika");
 		
 	}
 	
