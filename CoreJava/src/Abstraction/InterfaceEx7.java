5975ae6cd1cf589a02914aca22350bc5d2710da4
unknown <chogalesanika8@gmail.com>
2024-10-05 19:19:29 +0530

    initial

--- CoreJava/src/Abstraction/InterfaceEx7.java
+++ CoreJava/src/Abstraction/InterfaceEx7.java
@@ -9,6 +9,11 @@ interface Practice1
 {
 	void printName(String name);
 }
+@FunctionalInterface
+interface addnumbers
+{
+	int add(int a,int b);
+}
 public class InterfaceEx7 {
 
 	public static void main(String[] args) {
@@ -35,6 +40,10 @@ public class InterfaceEx7 {
 		
 		Practice1 p1=(name)->System.out.println("my name is:"+name);
 		p1.printName("sanika");
+		
+		addnumbers num=(a,b)->a+b;
+		int sum=num.add(10,20);
+		System.out.println(sum);
 		
 	}
 	
