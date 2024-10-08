6b5a537f20eba7277794504e4adefeff5b015790
unknown <chogalesanika8@gmail.com>
2024-10-08 20:31:13 +0530

    initial

---
+++ CoreJava/src/StringExamples/Example8.java
@@ -0,0 +1,14 @@
+package StringExamples;
+
+public class Example8 {
+
+	public static void main(String[] args) {
+		//trime method removes starting and ending space,bech ka space nahi niklega
+		System.out.println("sa        n         i    ka        ".trim()+"chogale");
+		System.out.println("====================");
+		System.out.println("riya".length());
+		System.out.println("====================");
+		System.out.println("siddhesh".charAt(2));
+	}
+	
+}
