052056f3675c03d87f39766909fd25bc317d678a
unknown <chogalesanika8@gmail.com>
2024-10-08 08:13:40 +0530

    initial

--- CoreJava/src/StringExamples/Example5.java
+++ CoreJava/src/StringExamples/Example5.java
@@ -3,13 +3,23 @@ package StringExamples;
 public class Example5 {
 
 	public static void main(String[] args) {
-		String s1="Mobile";
+		String s1="MooooooooOObile";
+		int count=0;
 		
 		for(int i=(s1.length()-1);i>=0;i--)
 		{
+			
 			System.out.print(s1.charAt(i));
 		}
+		System.out.println("===========================");
 		
+		for(char ch:s1.toCharArray())
+		{
+			if(ch=='o' || ch=='O')
+				count++;	
+		}
+		System.out.println(count);
+	
 	}
 	
 }
