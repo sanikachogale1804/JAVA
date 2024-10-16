346095a3e8169136c76688e1c7919886d640ab50
unknown <chogalesanika8@gmail.com>
2024-10-16 18:46:05 +0530

    initial

---
+++ src/Abstraction/AbstractionExample1.java
@@ -0,0 +1,14 @@
+package Abstraction;
+
+public class AbstractionExample1 {
+
+	public static void main(String[] args) {
+		
+		TV t=new TV();
+		new TV().start();
+		new TV().stop();
+		t.start();
+	}
+	
+	
+}
---
+++ src/Abstraction/Electronics.java
@@ -0,0 +1,20 @@
+package Abstraction;
+
+abstract class Electronics {
+
+	abstract void start();
+	abstract void stop();
+	
+}
+
+class TV extends Electronics{
+
+	public void start() {
+		System.out.println("start tv");
+	};
+	
+	public void stop() {
+		System.out.println("stopeed tv");
+	};
+	
+}
