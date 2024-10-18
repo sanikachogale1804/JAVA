ef5655cc91c85a2ad46de66e6a40a281409f0a04
unknown <chogalesanika8@gmail.com>
2024-10-18 20:54:11 +0530

    initial

--- src/classesAndObjects/class3.java
+++ src/classesAndObjects/class3.java
@@ -8,6 +8,22 @@ public class class3 {
 		
 		System.out.println(e1.printid(101));
 		
+		char alphabets[]= {'a','g','e','a','i','p','o','u'};
+		for(char ch:alphabets)
+		{
+			System.out.println(ch);
+		}
+		
+		int count=0;
+		for(char ch1:alphabets)
+		{
+			if(ch1=='a')
+			{
+				count++;
+			}
+		}
+		System.out.println("count"+count);
+		
 	}
 	
 }
