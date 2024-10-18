2e2a1c1c803eb53d1ea3a75766b332ff66edd6d0
unknown <chogalesanika8@gmail.com>
2024-10-18 19:28:09 +0530

    initial

---
+++ src/ArrayPractice/Array1.java
@@ -0,0 +1,22 @@
+package ArrayPractice;
+
+public class Array1 {
+
+	public static void main(String[] args) {
+		int numbers[]=new int[10];
+		numbers[0]=56;
+		numbers[1]=65;
+		numbers[2]=45;
+		numbers[3]=53;
+		numbers[4]=98;
+		numbers[5]=76;
+		numbers[6]=10;
+		numbers[7]=78;
+		numbers[8]=89;
+		numbers[9]=56;
+		
+		System.out.println(numbers[0]);
+		System.out.println(numbers[8]);
+	}
+
+}
