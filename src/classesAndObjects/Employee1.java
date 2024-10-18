56547b2ff92365331eb4fe9c546fa2bcbcbdc675
unknown <chogalesanika8@gmail.com>
2024-10-18 20:49:30 +0530

    initial

---
+++ src/classesAndObjects/Employee1.java
@@ -0,0 +1,23 @@
+package ClassesAndObjects;
+
+public class Employee1 {
+
+	int id;
+	String name;
+	int salary;
+	
+	void setdetails(int id,String name,int salary) {
+		this.id=id;
+		this.name=name;
+		this.salary=salary;
+	}
+	
+	public int printid(int id)
+	{
+		return id=id;
+	}
+}
+
+
+
+
---
+++ src/classesAndObjects/class3.java
@@ -0,0 +1,13 @@
+package ClassesAndObjects;
+
+public class class3 {
+
+	public static void main(String[] args) {
+		Employee1 e1=new Employee1();
+		e1.setdetails(1,"sanika",101);
+		
+		System.out.println(e1.printid(101));
+		
+	}
+	
+}
