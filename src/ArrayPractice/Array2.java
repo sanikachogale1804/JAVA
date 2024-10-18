a253bc10dd339f2a44f52a749c258330d539e489
unknown <chogalesanika8@gmail.com>
2024-10-18 19:47:32 +0530

    initial

--- src/ArrayPractice/Array2.java
+++ src/ArrayPractice/Array2.java
@@ -6,12 +6,13 @@ public class Array2 {
 
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
-		String weekdays[]= {"mon","tue","thur","fri","sat","sun"};
-		System.out.println(weekdays.length);
-		
-		for(String days:weekdays)
-		{
-			System.out.println(days);
-		}
+	    int primeNumbers[]= {11,13,17,23};
+	    System.out.println(primeNumbers.length);
+	    
+	    for(int num:primeNumbers)
+	    {
+	    	System.out.println(num);
+	    }
+	    
 	}
 }
