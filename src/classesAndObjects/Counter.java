2ed696130d40590bc49b732f9a587cdcf0e29a7f
unknown <chogalesanika8@gmail.com>
2024-10-18 21:01:59 +0530

    initial

---
+++ src/classesAndObjects/Counter.java
@@ -0,0 +1,12 @@
+package ClassesAndObjects;
+
+public class Counter {
+
+	static int count;
+	
+	Counter(){
+		count++;
+		System.out.println("count:"+count);
+	}
+	
+}
---
+++ src/classesAndObjects/CounterProgram.java
@@ -0,0 +1,14 @@
+package ClassesAndObjects;
+
+public class CounterProgram {
+
+	public static void main(String[] args) {
+		new Counter();
+		new Counter();
+		new Counter();
+		new Counter();
+		
+		
+	}
+	
+}
