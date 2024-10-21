8bd18f5fe76426c7f31402b539f8a5e320add375
unknown <chogalesanika8@gmail.com>
2024-10-21 22:32:16 +0530

    initial

--- LearnCollection/ListExample9.java
+++ LearnCollection/ListExample9.java
@@ -7,6 +7,7 @@ public class ListExample9 {
 	public static void main(String[] args) {
 		
 		Stack<Integer> stack=new Stack<Integer>();
+		//System.out.println(stack.empty());
 		stack.push(20);
 		stack.push(45);
 		stack.push(67);
@@ -26,6 +27,7 @@ public class ListExample9 {
 		System.out.println(stack.peek());
 		System.out.println(stack);
 		
+		System.out.println(stack.empty());
 	}
 	
 }
