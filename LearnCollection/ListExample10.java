b85a6721be1f8b13207bc71aa71340c175048b06
unknown <chogalesanika8@gmail.com>
2024-10-21 22:36:33 +0530

    initial

--- LearnCollection/ListExample10.java
+++ LearnCollection/ListExample10.java
@@ -1,9 +1,16 @@
 package LearnCollection;
 
+import java.util.HashSet;
+
 public class ListExample10 {
 
 	public static void main(String[] args) {
-		
+	
+		HashSet<Integer> hashset=new HashSet<Integer>();
+		hashset.add(30);
+		hashset.add(56);
+		hashset.add(90);
+		hashset.add(89);
 	}
 	
 }
