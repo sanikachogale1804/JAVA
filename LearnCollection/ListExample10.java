ea77745c936dc70c71c80846b4d2ff290a590a59
unknown <chogalesanika8@gmail.com>
2024-10-21 22:40:22 +0530

    initial

--- LearnCollection/ListExample10.java
+++ LearnCollection/ListExample10.java
@@ -1,6 +1,8 @@
 package LearnCollection;
 
+import java.util.Arrays;
 import java.util.HashSet;
+import java.util.List;
 
 public class ListExample10 {
 
@@ -8,12 +10,16 @@ public class ListExample10 {
 	
 		HashSet<Integer> hashset=new HashSet<Integer>();
 		hashset.add(30);
+		hashset.add(30);
 		hashset.add(56);
 		hashset.add(90);
 		hashset.add(89);
 		hashset.add(null);
 		
 		System.out.println(hashset);
+		
+		List<Integer> list=Arrays.asList(12,20,20,78,98,98);
+		System.out.println(list);
 	}
 	
 }
