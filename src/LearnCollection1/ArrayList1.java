85510c294d41f4f49d50b6d5836c7bd5f31875c9
unknown <chogalesanika8@gmail.com>
2024-10-24 19:02:53 +0530

    initial

---
+++ src/LearnCollection1/ArrayList1.java
@@ -0,0 +1,19 @@
+package LearnCollection1;
+
+import java.util.ArrayList;
+
+public class ArrayList1 {
+
+	public static void main(String[] args) {
+		ArrayList list=new ArrayList<>();
+		list.add(10);
+		list.add("Sanika");
+		list.add(20.20);
+		list.add(true);
+		list.add(null);
+		list.add(10);
+		
+		System.out.println(list);
+	}
+	
+}
