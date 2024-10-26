ab9523663db3865d18686951d8e2f90f8058ba19
unknown <chogalesanika8@gmail.com>
2024-10-26 14:33:47 +0530

    initial

--- StreamAPI/StreamExample1.java
+++ StreamAPI/StreamExample1.java
@@ -1,9 +1,16 @@
 package StreamAPI;
 
+import java.util.ArrayList;
+
 public class StreamExample1 {
 	
 	public static void main(String[] args) {
-		
+		ArrayList<Integer> list=new ArrayList<Integer>();
+		for(int i=1;i<=20;i++)
+		{
+			list.add(i);
+		}
+		System.out.println(list);
 	}
 
 }
