d4712a749ba6dce3c23167b5d3f0a845d98642bc
unknown <chogalesanika8@gmail.com>
2024-10-27 19:01:32 +0530

    initial

--- GenericsAndFunctionalInterface/lambdaPractice.java
+++ GenericsAndFunctionalInterface/lambdaPractice.java
@@ -6,12 +6,20 @@ import java.util.function.Consumer;
 import java.util.function.Predicate;
 import java.util.stream.Stream;
 
+@FunctionalInterface
+interface A
+{
+	void display();
+	
+}
 
 
 public class lambdaPractice {
 
 	public static void main(String[] args) {
-		List<Integer> numbers=Arrays.asList(10,70,67,54,97,69,45,29,35,19);
+//		List<Integer> numbers=Arrays.asList(10,70,67,54,97,69,45,29,35,19);
+//		numbers.stream().forEach((n)->System.out.println(n%2==0));
+		
 		
         
 	}
