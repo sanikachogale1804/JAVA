e24704434bed0505dd4e1a4c57300047fa8c5182
unknown <chogalesanika8@gmail.com>
2024-10-27 14:43:12 +0530

    initial

--- GenericsAndFunctionalInterface/lambdaPractice.java
+++ GenericsAndFunctionalInterface/lambdaPractice.java
@@ -7,15 +7,17 @@ import java.util.function.Predicate;
 import java.util.stream.Stream;
 
 @FunctionalInterface
-interface animal
+interface shape
 {
 	int square(int a,int b);
 }
+
 public class lambdaPractice {
 
 	public static void main(String[] args) {
-		
-        
+		shape squ=(a,b)->a*b;
+		int ans=squ.square(10, 20);
+        System.out.println(ans);
 	}
 	
 }
