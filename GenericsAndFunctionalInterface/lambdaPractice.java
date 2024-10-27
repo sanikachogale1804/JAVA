067a9a88cf5fda57bba158d9bbb766d70fe71a3b
unknown <chogalesanika8@gmail.com>
2024-10-27 14:23:00 +0530

    initial

--- GenericsAndFunctionalInterface/lambdaPractice.java
+++ GenericsAndFunctionalInterface/lambdaPractice.java
@@ -6,13 +6,30 @@ import java.util.function.Consumer;
 import java.util.function.Predicate;
 import java.util.stream.Stream;
 
-
+abstract class product
+{
+	abstract void details();
+}
+class mobile extends product
+{
+	void details()
+	{
+		System.out.println("Mobile");
+	}
+}
+class Tv extends product
+{
+	void details()
+	{
+		System.out.println("Tv");
+	}
+}
 
 public class lambdaPractice {
 
 	public static void main(String[] args) {
-		
-	
+		new mobile().details();
+        new Tv().details();	
 	}
 	
 }
