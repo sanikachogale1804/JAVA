3909f27c7a5df58d2face558e25c9b9db246879d
unknown <chogalesanika8@gmail.com>
2024-10-28 14:31:09 +0530

    initial

--- StreamAPI/StreamExample7.java
+++ StreamAPI/StreamExample7.java
@@ -2,12 +2,15 @@ package StreamAPI;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Consumer;
 
 public class StreamExample7 {
 	
 	public static void main(String[] args) {
 		List<String> list = Arrays.asList("sanika","sneha","riya","akshata","tushar");
-		System.out.println(list);
+		list.stream().forEach(n->System.out.println(n.length()));
+		
+		
 	}
 
 }
