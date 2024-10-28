ad0a67b7a12d1b2d8c5b137ef26edb57177928d7
unknown <chogalesanika8@gmail.com>
2024-10-28 07:45:07 +0530

    initial

--- StreamAPI/StreamExample3.java
+++ StreamAPI/StreamExample3.java
@@ -3,6 +3,8 @@ package StreamAPI;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 public class StreamExample3 {
 	
@@ -18,8 +20,22 @@ public class StreamExample3 {
 		//foreach filter ne return kiye hue stream pe lagaya hai
 		//n->n>20=test method ka implementation
 		//filter ko bola greater than number chahiye and foreach ko bola vo print karo
+		//filter true ya false me ans dega(filter bas check karne ka kam karega)
 		//jis bhi method ka return type void hota hai usko koi method nahi aaply kar sakte
+		//numbers.stream() stream ka object milta hai matlab new stream milti hai and uske upar filter lagaya
 		numbers.stream().filter(n->n>20).forEach(n->System.out.println(n));
+		
+		//collect method ka kam hai apko return me kya chahiye (set,list)
+		//collect method ka kam hai stream ke andar ke ek ek element ko print karna or usko list dena
+		//collect collector ka object demand karta hai
+		//collectors(class) ka return type collector hai
+		//without instanse variable usko static bolte hai
+		System.out.println("=======================");
+		//Collectors.toList(); giving me collector ka object
+		//collectors class ki tolist method collect ka object deri hai
+	    //Collector<Object, ?, List<Object>> list = Collectors.toList();
+		List<Integer> greaterthan20 = numbers.stream().filter(n->n>20).collect(Collectors.toList());
+		System.out.println(greaterthan20);
 		
 	}
 
