8a788c08802fea73ea6750114ee32f32956a7c2a
unknown <chogalesanika8@gmail.com>
2024-10-28 08:29:12 +0530

    initial

--- StreamAPI/StreamExample5.java
+++ StreamAPI/StreamExample5.java
@@ -2,6 +2,8 @@ package StreamAPI;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class StreamExample5 {
 	
@@ -11,7 +13,13 @@ public class StreamExample5 {
 		//map return me value dega foreach return me nahi dega
 		
 		List<Integer> numbers = Arrays.asList(12,79,67,34,90,13);
-		
+		Function<Integer, Integer> squarefunction=(n)->n*n;
+		List<Integer> square = numbers.stream().map(squarefunction).collect(Collectors.toList());
+		//function ka return type aap batate ho
+		//map ko functions ka object chahiye
+		//foreach ka return type void tha and map ka retrun type stream hai
+		//dono alag alag interface accept karte hai
+		System.out.println(square);
 		
 	}
 
