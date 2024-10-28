4cf6a3704e470d2030776d6738c32411e41362a2
unknown <chogalesanika8@gmail.com>
2024-10-28 22:51:16 +0530

    initial

--- StreamAPI/StreamExample7.java
+++ StreamAPI/StreamExample7.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.function.Consumer;
 import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class StreamExample7 {
 	
@@ -12,7 +13,8 @@ public class StreamExample7 {
 		
 		List<Integer> numbers = Arrays.asList(12,79,67,45,98,34);
 		Function<Integer, Integer> squarefunction=n->n*n;
-		numbers.stream().map(squarefunction);
+		List<Integer> square = numbers.stream().map(squarefunction).collect(Collectors.toList());
+		System.out.println(square);
 	}
 
 }
