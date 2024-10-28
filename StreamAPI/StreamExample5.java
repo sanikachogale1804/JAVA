35fbceaf90cad906af7b557f075b1f6782b01007
unknown <chogalesanika8@gmail.com>
2024-10-28 08:20:40 +0530

    initial

---
+++ StreamAPI/StreamExample5.java
@@ -0,0 +1,18 @@
+package StreamAPI;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class StreamExample5 {
+	
+	public static void main(String[] args) {
+		//map stream ko belong karta hai
+		//har object ke upar kuch kuch na preform karna hai to map use karenge kuynki actions should be prformed each object
+		//map return me value dega foreach return me nahi dega
+		
+		List<Integer> numbers = Arrays.asList(12,79,67,34,90,13);
+		
+		
+	}
+
+}
