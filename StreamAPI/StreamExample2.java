879307111b2d0624951bb0ff96bd025d70e82739
unknown <chogalesanika8@gmail.com>
2024-10-28 14:52:51 +0530

    initial

--- StreamAPI/StreamExample2.java
+++ StreamAPI/StreamExample2.java
@@ -11,6 +11,8 @@ public class StreamExample2 {
 	public static void main(String[] args) {
 		List<Integer> list=Arrays.asList(10,78,89,67,56,45);
 		list.stream().filter(n->n%2==0).forEach(n->System.out.println(n));
+        List<Integer> greaterthan20 = list.stream().filter(n->n>20).collect(Collectors.toList());
+        System.out.println(greaterthan20);
 		
 	    List<String> names=Arrays.asList("Sanika","Sneha","akshata","tushar");
 	    
