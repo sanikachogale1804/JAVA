d2b71a42e1cd5fcdcd5f578d13d124d2bb48f282
unknown <chogalesanika8@gmail.com>
2024-10-29 22:02:02 +0530

    initial

--- StreamAPI/StreamExample7.java
+++ StreamAPI/StreamExample7.java
@@ -11,16 +11,7 @@ import java.util.stream.Collectors;
 public class StreamExample7 {
 	
 	public static void main(String[] args) {
-//		List<Integer> numbers = Arrays.asList(12,79,67,45,98,34);
-		List<Integer> numbers = Arrays.asList();
-	
-		Optional<Integer> first = numbers.stream().findFirst();
-		System.out.println(first);
 		
-//		Integer integer = first.get();
-//		System.out.println(integer);
-		
-		Integer i = first.orElseThrow(()->new RuntimeException("No element present"));
-		System.out.println(i);
+		List<Object> numbers = Arrays.asList();
 	}
 }
