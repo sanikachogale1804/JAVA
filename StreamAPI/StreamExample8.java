d6aeddde5e143c4e579bc3e3e10e426d942edca3
unknown <chogalesanika8@gmail.com>
2024-10-29 08:01:05 +0530

    initial

--- StreamAPI/StreamExample8.java
+++ StreamAPI/StreamExample8.java
@@ -1,11 +1,20 @@
 package StreamAPI;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
 public class StreamExample8 {
 	
 	public static void main(String[] args) {
 		
-		
-		
+		List<Integer> numbers = Arrays.asList(12,79,67,45,98,34);
+		Optional<Integer> first = numbers.stream().findFirst();
+		//opional ek object hai to hum uspe kuch operation perform nahi kar sakte(matlab addition,subtraction) is liye hum get() ya orElsethrow method use kiye
+		System.out.println(first);
+		Integer integer = first.get();
+		System.out.println(integer);
 	}
 
 }
