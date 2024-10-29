89aef6e1549a25ee299a112607221dbb65413004
unknown <chogalesanika8@gmail.com>
2024-10-29 21:54:05 +0530

    initial

--- StreamAPI/StreamExample7.java
+++ StreamAPI/StreamExample7.java
@@ -11,6 +11,11 @@ import java.util.stream.Collectors;
 public class StreamExample7 {
 	
 	public static void main(String[] args) {
+		List<Integer> numbers = Arrays.asList(12,79,67,45,98,34);
+		Optional<Integer> first = numbers.stream().findFirst();
+		System.out.println(first);
 		
+		Integer integer = first.get();
+		System.out.println(integer);
 	}
 }
