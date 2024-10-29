1f50dc311d02c339b42bf7b578f7bd634a76ca52
unknown <chogalesanika8@gmail.com>
2024-10-29 08:47:46 +0530

    initial

--- StreamAPI/StreamExample9.java
+++ StreamAPI/StreamExample9.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
+import java.util.function.Predicate;
 
 public class StreamExample9 {
 	
@@ -22,11 +23,14 @@ public class StreamExample9 {
 //		parellelpipeline matlab ek pipeline nahi bhohot jyada pipeline(data alag alag pipeline se milega)
 //		findAny ek pipeline ke upar nahi kam karta 
 //		parellelstream me sequence ka guarante nahi hai
-		
+//		map,filter sari chije parallel pe apply hogi par sequnce ka guarantee nahi hogi
 		System.out.println("===================================");
 		
 		numbers.parallelStream().forEach((num)->System.out.println(num));
+		System.out.println("===================================");
 		
+		boolean evenMatch = numbers.stream().anyMatch((n)->n%2==0);
+		System.out.println(evenMatch);
 	}
 
 }
