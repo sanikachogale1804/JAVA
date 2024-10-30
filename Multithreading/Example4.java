7af63fe388f9a32653e75846ff0f0b1ab0645a2a
unknown <chogalesanika8@gmail.com>
2024-10-30 22:13:43 +0530

    initial

--- Multithreading/Example4.java
+++ Multithreading/Example4.java
@@ -6,36 +6,13 @@ class T6 extends Thread
 {
 	public void run()
 	{
-		for(int i=1;i<=10;i++)
+		while(true)
 		{
-			System.out.println("*");
-			try {
-				Thread.sleep(1000);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+			
 		}
 	}
 }
 
-class T7 extends Thread
-{
-	public void run()
-	{
-		for(int i=1;i<=10;i++)
-		{
-			System.out.println("$");
-			try {
-				Thread.sleep(1000);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
-		
-	}
-}
 
 public class Example4 {
 	
@@ -44,8 +21,9 @@ public class Example4 {
 		T6 t6=new T6();
 		t6.start();
 		
-		T7 t7=new T7();
-		t7.start();
+		
+		
+		
 	}
 
 }
