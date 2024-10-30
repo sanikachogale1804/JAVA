78c585f5d3296ca376ec26c7cca48c66b4c92b87
unknown <chogalesanika8@gmail.com>
2024-10-30 07:34:32 +0530

    initial

---
+++ Multithreading/Example1.java
@@ -0,0 +1,23 @@
+package Multithreading;
+
+class T1 extends Thread
+{
+	public void run()
+	{
+		for(int i=1;i<=10;i++)
+		{
+			System.out.println("sanika");
+		}
+	}
+	
+}
+
+public class Example1 {
+	
+	public static void main(String[] args) {
+		
+		//first create a classs then inherit Thread
+		
+	}
+
+}
