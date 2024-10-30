3283cf701fbe4c8b7f5c1664e3c3e8f34ed73f49
unknown <chogalesanika8@gmail.com>
2024-10-30 22:10:22 +0530

    initial

--- Multithreading/Example4.java
+++ Multithreading/Example4.java
@@ -26,6 +26,12 @@ class T7 extends Thread
 		for(int i=1;i<=10;i++)
 		{
 			System.out.println("by-by");
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
 		
 	}
