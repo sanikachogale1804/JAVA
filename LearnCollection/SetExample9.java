90a92d487b45aa8bdcde2f83f8a4e5a61d9edf19
unknown <chogalesanika8@gmail.com>
2024-10-31 20:24:13 +0530

    initial

--- LearnCollection/SetExample9.java
+++ LearnCollection/SetExample9.java
@@ -1,8 +1,15 @@
 package LearnCollection;
 
+import java.util.HashSet;
+
 public class SetExample9 {
 	
 	public static void main(String[] args) {
+		HashSet<Integer> set=new HashSet<Integer>();
+		set.add(67);
+		set.add(190);
+		set.add(70);
+		set.add(43);
 		
 	}
 
