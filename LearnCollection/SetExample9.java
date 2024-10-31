cc60b982da3d45e1e5ccc46969ac1cd1929a5030
unknown <chogalesanika8@gmail.com>
2024-10-31 20:49:34 +0530

    initial

--- LearnCollection/SetExample9.java
+++ LearnCollection/SetExample9.java
@@ -14,6 +14,10 @@ public class SetExample9 {
 	public static void main(String[] args) {
 	
 		TreeSet<Integer> treeset=new TreeSet<Integer>();
-		
+		treeset.add(78);
+		treeset.add(900);
+		treeset.add(100);
+		treeset.add(200);
+		System.out.println(treeset);
 }
 }
