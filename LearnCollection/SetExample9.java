3c280cfd2b58c926e38ebbe95dccf16bc22fd9bd
unknown <chogalesanika8@gmail.com>
2024-10-31 20:36:54 +0530

    initial

--- LearnCollection/SetExample9.java
+++ LearnCollection/SetExample9.java
@@ -2,24 +2,17 @@ package LearnCollection;
 
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Scanner;
 
 public class SetExample9 {
 	
 	public static void main(String[] args) {
-	Scanner sc=new Scanner(System.in);
-	HashSet<String> colors=new HashSet<String>();
 	
-	
-	for(int i=1;i<=5;i++)
-	{
-		String color=sc.next();	
-		colors.add(color);
-	}
-System.out.println(colors);
-
-colors.clear();
-System.out.println(colors);
+		LinkedHashSet<Integer> list=new LinkedHashSet<Integer>();
+		
+		
 }
 }
