79d9c78484546b90834d24c67b27c403394f19fe
unknown <chogalesanika8@gmail.com>
2024-10-31 20:48:23 +0530

    initial

--- LearnCollection/SetExample9.java
+++ LearnCollection/SetExample9.java
@@ -7,36 +7,13 @@ import java.util.LinkedHashMap;
 import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Scanner;
+import java.util.TreeSet;
 
 public class SetExample9 {
 	
 	public static void main(String[] args) {
 	
-		LinkedHashSet<Integer> set=new LinkedHashSet<Integer>();
-		set.add(10);
-		set.add(78);
-		set.add(10);
-		set.add(45);
-		set.add(900);
-		set.add(2);
-		System.out.println(set);
-		
-		System.out.println(set.size());
-		System.out.println("===============");
-		
-		Iterator<Integer> iterator=set.iterator();
-		System.out.println(set);
-		
-		System.out.println(iterator.next());
-		System.out.println(iterator.next());
-		
-		System.out.println("===============");
-		while (iterator.hasNext()) {
-			System.out.println(iterator.next());
-		}
-		System.out.println(iterator);
-		
-		System.out.println(set.isEmpty());
+		TreeSet<Integer> treeset=new TreeSet<Integer>();
 		
 }
 }
