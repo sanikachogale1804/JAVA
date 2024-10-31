73ca1c8722e6bebed0a95af1977ba7dcd985d034
unknown <chogalesanika8@gmail.com>
2024-10-31 20:32:56 +0530

    initial

--- LearnCollection/SetExample9.java
+++ LearnCollection/SetExample9.java
@@ -3,12 +3,15 @@ package LearnCollection;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Scanner;
 
 public class SetExample9 {
 	
 	public static void main(String[] args) {
+	Scanner sc=new Scanner(System.in);
 	HashSet<String> colors=new HashSet<String>();
 	
-	}
+	String color=sc.next();	
 
+}
 }
