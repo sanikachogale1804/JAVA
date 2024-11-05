1c55f831492f8dae2e37a1da069cd4e48a20211d
unknown <chogalesanika8@gmail.com>
2024-11-05 07:43:28 +0530

    initial

--- Multithreading/Example5.java
+++ Multithreading/Example5.java
@@ -12,9 +12,20 @@ class Thread1 implements Runnable
 public class Example5 {
 	
 	public static void main(String[] args) {
-		
+		//we want start method or start method Thread ke pass hai is liye Thread ka object banaya
 		Thread t1=new Thread(new Thread1());
 		t1.start();
+		
+		
+	    Runnable r1=new Runnable() {
+			public void run()
+			{
+				System.out.println("Anonymous Inner Class");
+			}
+		};
+		
+		Thread t2=new Thread(r1);
+		t2.start();
 	}
 
 }
