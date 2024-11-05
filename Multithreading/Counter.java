c82dc69a47551fe9c32188d7dcad17746205ff09
unknown <chogalesanika8@gmail.com>
2024-11-05 08:28:01 +0530

    initial

--- Multithreading/Counter.java
+++ Multithreading/Counter.java
@@ -2,10 +2,11 @@ package Multithreading;
 
 public class Counter {
 
-		//Synchronized-at a time only one thread
+		//Synchronized-at a time only one thread(matlab ek thread ek hi time pe aayegi )
+	
 		int count=0;
 		
-        public void increment()
+        synchronized public void increment()
         {
         	this.count++;
         }
--- Multithreading/Example7.java
+++ Multithreading/Example7.java
@@ -2,8 +2,31 @@ package Multithreading;
 
 public class Example7 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		
+		Counter counter = new Counter();
+		Thread t1=new Thread(()->{
+			for(int i=1;i<=500;i++)
+			{
+				counter.increment();
+			}
+		});
+		
+		Thread t2=new Thread(()->{
+			for(int i=1;i<=500;i++)
+			{
+				counter.increment();
+			}
+		});
+		
+		t1.start();
+		t2.start();
+		
+		t1.join();
+		t2.join();
+		
+		//main method rukegi nahi t1 and t2 ka kam hone tak is liye pehle 0 print kiya
+		System.out.println(counter.count);
 	}
 	
 }
