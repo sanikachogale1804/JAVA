d1a561cfdf3fd9c96e30d1873282cf473d259de9
unknown <chogalesanika8@gmail.com>
2024-11-05 07:30:15 +0530

    initial

---
+++ Multithreading/Example5.java
@@ -0,0 +1,20 @@
+package Multithreading;
+
+class Thread1 implements Runnable
+{
+	@Override
+	public void run() {
+	   System.out.println("Thread 1");
+	}
+	
+}
+
+public class Example5 {
+	
+	public static void main(String[] args) {
+		
+		
+		
+	}
+
+}
